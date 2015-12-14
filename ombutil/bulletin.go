636a051c46454137d111333b78bc3ea458142fb4
Ubuntu <nskelsey@gmail.com>
2015-12-14 15:42:15 +0000

    Fixed Tag Parser, updated tests

--- ombutil/bulletin.go
+++ ombutil/bulletin.go
@@ -1,6 +1,9 @@
 package ombutil
 
 import (
+	"errors"
+	"unicode/utf8"
+
 	"github.com/btcsuite/btcd/chaincfg"
 	"github.com/btcsuite/btcd/txscript"
 	"github.com/btcsuite/btcd/wire"
@@ -59,6 +62,9 @@ func (bltn *Bulletin) AddBlock(blk *btcutil.Block) {
 
 // Returns the "Author" who signed the first txin of the transaction
 func parseAuthor(tx *wire.MsgTx, net *chaincfg.Params) (Author, error) {
+	if len(tx.TxIn) < 1 {
+		return Author(""), errors.New("No TxIns, malformed Bitcoin transaction")
+	}
 	sigScript := tx.TxIn[0].SignatureScript
 
 	dummyTx := wire.NewMsgTx()
@@ -82,13 +88,56 @@ func parseAuthor(tx *wire.MsgTx, net *chaincfg.Params) (Author, error) {
 	return Author(addrPubKey.EncodeAddress()), nil
 }
 
+// ParseTags returns up to maxNum tags in the passed string. Tags are pulled
+// out in iterative order and they are started with a '#' and concluded with a
+// tag break character.
+func ParseTags(m string) []Tag {
+	maxNum := 5
+	tags := make([]Tag, 0, maxNum)
+	var r rune
+	var i_s int = 0
+
+	for i := 0; i < len(m); i += i_s {
+		r, i_s = utf8.DecodeRuneInString(m[i:])
+		if r == '#' {
+			var j_s int
+			var v rune
+			var j int
+			for j = i; j < len(m); j += j_s {
+				v, j_s = utf8.DecodeRuneInString(m[j:])
+				if isTagBreak(v) {
+					break
+				}
+			}
+			tag := Tag(m[i:j])
+			//fmt.Printf("i: %d, j: %d, tag: %s\n", i, j, tag)
+			if len(tags) >= maxNum {
+				break
+			}
+			tags = append(tags, tag)
+			i = j
+			i_s = 0
+		}
+	}
+	return tags
+}
+
 // Tags returns all of the tags encoded within the message body of the
 // bulletin. Only the first 5 tags are counted and returned.
 func (bltn *Bulletin) Tags() []Tag {
-	t := []Tag{
-		Tag("#foo"),
-		Tag("#bar"),
-		Tag("#baz"),
+	m := bltn.Wire.GetMessage()
+	return ParseTags(m)
+}
+
+var tagBreaks []rune = []rune{' ', '\f', '\n', '\r', '\t', '\v', '\u00a0', '\u1680', '\u180e', '\u2000', '-', '\u200a', '\u2028', '\u2029', '\u202f', '\u205f', '\u3000', '\ufeff'}
+
+// isTagBreak returns true if the passed rune is one of the acknowledge tag
+// seperator characters.
+func isTagBreak(r rune) bool {
+	for i := range tagBreaks {
+		if r == tagBreaks[i] {
+			return true
+		}
 	}
-	return t
+	return false
 }
--- ombutil/bulletin_test.go
+++ ombutil/bulletin_test.go
@@ -3,21 +3,75 @@ package ombutil_test
 import (
 	"testing"
 
-	"github.com/soapboxsys/ombudslib/protocol/ombproto"
+	"github.com/btcsuite/btcd/chaincfg"
+	"github.com/btcsuite/btcd/wire"
+	. "github.com/soapboxsys/ombudslib/ombutil"
 )
 
-func TestBulletinCreate(t *testing.T) {
-	topic := "King Arthur Arrives in Camelot"
-	msg := "What knight live in that castle over there?"
+// Challenge IsBulletin with several blks of transactions that are not
+// bulletins.
+func TestIsNotBulletin(t *testing.T) {
 
-	bltn, err := ombproto.NewBulletinFromStr("nick", topic, msg)
-	if err != nil {
-		t.Errorf("New failed with: %v", err)
-		return
+}
+
+func TestIsBulletin(t *testing.T) {
+
+}
+
+func TestBulletinParse(t *testing.T) {
+
+	// Not enough TxIns
+	tx := wire.NewMsgTx()
+
+	_, err := NewBulletin(tx, &chaincfg.MainNetParams)
+	if err == nil {
+		t.Fatalf("Creating the bltn should have failed: %s", err)
 	}
 
-	if bltn.Message != msg {
-		t.Errorf("Msgs do not match: %v", err)
-		return
+	// Not a Bulletin
+
+	tx = chaincfg.MainNetParams.GenesisBlock.Transactions[0]
+	_, err = NewBulletin(tx, &chaincfg.MainNetParams)
+	if err == nil {
+		t.Fatalf("Creating bltn from coinbase should fail")
 	}
+}
+
+func TestTagParse(t *testing.T) {
+	m1 := "#This #is #a #default #Message"
+	t1 := ParseTags(m1)
+
+	ts := []Tag{Tag("#This"), Tag("#is"), Tag("#a"), Tag("#default"), Tag("#Message")}
+	if !sameTags(t1, ts) {
+		t.Fatalf("Parsed :%b, Wanted :%b", t1, ts)
+	}
+
+	m2 := "#AVeryLongTag"
+	t2 := ParseTags(m2)
+
+	if len(t2) != 1 {
+		ts = []Tag{Tag("#AVeryLongTag")}
+		t.Fatalf("Parsed: %b, Wanted: %b", t2, ts)
+	}
+
+	m3 := "#More #than #five #tags #are #here also#bad#tags #today"
+	t3 := ParseTags(m3)
+
+	ts = []Tag{Tag("#More"), Tag("#than"), Tag("#five"), Tag("#tags"), Tag("#are")}
+	if !sameTags(t3, ts) {
+		t.Fatalf("Parsed: %b, Wanted: %b", t3, ts)
+	}
+}
+
+// Utility function to see if the array of tags are exactly equal.
+func sameTags(a, b []Tag) bool {
+	if len(a) != len(b) {
+		return false
+	}
+	for i, a_i := range a {
+		if string(a_i) != string(b[i]) {
+			return false
+		}
+	}
+	return true
 }
