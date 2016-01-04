f4fac3828bd1cf8b44841aa4cb833e2c615c07e4
Ubuntu <nskelsey@gmail.com>
2016-01-04 22:49:15 +0000

    Removed code.google.p from deps

--- pubrecdb/setup.go
+++ pubrecdb/setup.go
@@ -9,7 +9,7 @@ import (
 	"github.com/btcsuite/btcd/chaincfg"
 	"github.com/btcsuite/btcutil"
 
-	"code.google.com/p/go-sqlite/go1/sqlite3"
+	"github.com/mxk/go-sqlite/sqlite3"
 )
 
 // The overarching struct that contains everything needed for a connection to a
