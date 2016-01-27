eef698f7e978adea22b28b177c3d1dddbc655996
Ubuntu <nskelsey@gmail.com>
2016-01-27 03:58:20 +0000

    verbosity increased!

--- cmds/rollbackchain/main.go
+++ cmds/rollbackchain/main.go
@@ -13,7 +13,6 @@ import (
 	"github.com/soapboxsys/ombudslib/pubrecdb"
 )
 
-var node = flag.String("node", "", "The node to connect to")
 var defaultDir = "/home/ubuntu/.ombnode/data"
 var dataDir = flag.String("datadir", defaultDir, "The nodes data directory")
 var testNet = flag.Bool("testnet", false, "The testnet switch")
@@ -34,7 +33,6 @@ func main() {
 
 	// Load pubrec
 	p := path.Join(dataPath, "pubrecord.db")
-	log.Println(p)
 	precdb, err := pubrecdb.LoadDB(p)
 	if err != nil {
 		log.Fatal("Loading prec: ", err)
@@ -43,35 +41,45 @@ func main() {
 	// Load Block chain
 	dbPath := path.Join(dataPath, "blocks_leveldb")
 	db, err := database.OpenDB("leveldb", dbPath)
+	defer db.Close()
 	if err != nil {
 		log.Fatal("Loading leveldb: ", err)
 	}
+
+	printDBStates(db, precdb)
 
 	t, err := db.FetchBlockHeaderBySha(target)
 	if err != nil {
 		log.Fatal("LevelDB fetch: ", err)
 	}
 
+	log.Println("Dropping leveldb blocks....")
+
 	// Disconnect all blocks after Sha
 	if err := db.DropAfterBlockBySha(&t.PrevBlock); err != nil {
 		log.Print("Leveldb Drop Threw: ", err)
 	}
 
+	log.Println("Dropping pubrecord blocks....")
 	// Delete from pubrec up to shahash.
 	if err = precdb.DropAfterBlockBySha(&t.PrevBlock); err != nil {
-		log.Print("Pubrec Drop Trew: ", err)
+		log.Print("Pubrec Drop Threw: ", err)
 	}
 
-	log.Println("Deleted all blocks after target sha")
+	log.Println("Deleted all blocks after target sha in both DBs")
+	printDBStates(db, precdb)
+}
+
+func printDBStates(db database.Db, prec *pubrecdb.PublicRecord) {
 	newest, h, err := db.NewestSha()
 	if err != nil {
-		log.Fatal(err)
+		log.Fatal("Leveldb newest: ", err)
 	}
-	log.Printf("Leveldb newest sha at height %d is: %s", h, newest.String())
 
-	tip, err := precdb.GetBlockTip()
+	log.Printf("Leveldb newest sha at height %d is: %s", h, newest.String())
+	tip, err := prec.GetBlockTip()
 	if err != nil {
-		log.Fatal(err)
+		log.Fatal("Pubrec get: ", err)
 	}
-	log.Printf("PubrecordDb newest sha at height %d is %s", tip.Head.Height, tip.Head.Hash)
+	log.Printf("Pubrecdb newest sha at height %d is: %s", tip.Head.Height, tip.Head.Hash)
 }
